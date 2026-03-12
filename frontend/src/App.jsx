648cb4cb40820615f26f663011af009a087689e9
Stu-me <stusundram@gmail.com>
2026-03-12 19:21:06 +0530

    folder structure created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-8xl m-1.5 caret-amber-600 bg-blue-300'>Hi there</div>
+    </>
+  )
+}
+
+export default App
